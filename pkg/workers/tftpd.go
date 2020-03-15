c2f5aa2d8c081aa15c62bccf966df8eab56db5c2
Felix Pojtinger <felix@pojtinger.com>
2020-03-15 11:06:56 +0100

    feat: Add tests for TFTP worker

--- pkg/workers/tftpd.go
+++ pkg/workers/tftpd.go
@@ -35,27 +35,17 @@ func (t *TFTPD) readHandler(filename string, rf io.ReaderFrom) error {
 	return nil
 }
 
+// Not implemented
 func (t *TFTPD) writeHandler(filename string, wt io.WriterTo) error {
-	file, err := os.OpenFile(filepath.Join(t.root, filename), os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
-	if err != nil {
-		return err
-	}
-
-	if _, err := wt.WriteTo(file); err != nil {
-		return err
-	}
-
 	return nil
 }
 
-// TODO: Add tests
 // Start starts the TFTP server and blocks until `Stop` is called
 func (t *TFTPD) Start() error {
 	t.instance = tftp.NewServer(t.readHandler, t.writeHandler)
 	return t.instance.ListenAndServe(t.bindAddress)
 }
 
-// TODO: Add tests
 // Stop stops the TFTP server
 func (t *TFTPD) Stop() {
 	t.instance.Shutdown()
